a8b84f270f7ed7a7147c4f1e6c8b2523371289dc
Aven Bross <kieroda@gmail.com>
2025-05-23 18:53:04 -0800

    Increased max_len for fuzzer, which was reduced with recent switch to address sanitizer

--- clang_fuzz.sh
+++ clang_fuzz.sh
@@ -1,4 +1,4 @@
 clang -fsanitize=undefined -fsanitize=unreachable -fsanitize-trap -fsanitize=fuzzer -fsanitize=address \
     -O2 -g3 -I ./include/ -I ./deps/libaven/include/ \
     fuzz/fmt.c -o fuzz_fmt
-./fuzz_fmt -max_len=4096 -jobs=4 -dict=fuzz/cdict.txt -timeout=1
+./fuzz_fmt -max_len=20480 -jobs=4 -dict=fuzz/cdict.txt -timeout=1
