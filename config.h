919134d3c32e299fe691131375600c9867877423
Aven Bross <kieroda@gmail.com>
2026-05-01 09:51:54 -0800

    updated to latest libaven

--- config.h
+++ config.h
@@ -1,14 +1,13 @@
 // config.h can be used to defines custom defaults for flags
 
 #if !defined(_WIN32) && defined(__GNUC__)
-   #define AVEN_BUILD_COMMON_DEFAULT_CCFLAGS \
-       "-std=c99 -pedantic -fstrict-aliasing -O0 -g3 -Werror -Wall -Wextra " \
-       "-Wshadow -Wconversion -Wdouble-promotion -Winit-self " \
-       "-Wcast-align -Wstrict-prototypes -Wold-style-definition " \
-       "-fsanitize-trap -fsanitize=unreachable -fsanitize=undefined " \
-       "-fsanitize=address"
-    #define AVEN_BUILD_COMMON_DEFAULT_LDFLAGS \
-       "-fsanitize=address"
+    #define AVEN_BUILD_COMMON_DEFAULT_CFLAGS "-std=c99"
+    #define AVEN_BUILD_COMMON_DEFAULT_CCFLAGS \
+                "-pedantic -fstrict-aliasing -O0 -g3 -Werror -Wall -Wextra " \
+                "-Wshadow -Wconversion -Wdouble-promotion -Winit-self " \
+                "-Wcast-align -Wstrict-prototypes -Wold-style-definition " \
+                "-fsanitize-trap -fsanitize=unreachable -fsanitize=undefined " \
+                "-fsanitize=address"
+    #define AVEN_BUILD_COMMON_DEFAULT_LDFLAGS "-fsanitize=address"
 #endif
-
 
